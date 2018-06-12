87042b4e1e0d13075442ad94156525450f5e4f9e
AlexKovalenkoBy <kovalenkoam@gmail.com>
2018-06-12 21:32:22 +0300

    //вот тут у меня какой-то косяк с видимостью метода...             Boolean MyrealDay = dayOfWeek2.Program.DayCalc(dtest);

--- DayOfWeek2Tests/UnitTest1.cs
+++ DayOfWeek2Tests/UnitTest1.cs
@@ -13,6 +13,8 @@ namespace DayOfWeek2Tests
             DateTime dtest="01.06.2018";
             Boolean ItsWorkday = true;
             //act 
+
+            //вот тут у меня какой-то косяк с видимостью метода...
             Boolean MyrealDay = dayOfWeek2.Program.DayCalc(dtest);
             //assert
             Assert.AreEqual(ItsWorkday, MyrealDay);
